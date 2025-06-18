2d837e094cac717e42dc5b5abfc22b85c2f2f649
Matthew Feickert <matthew.feickert@cern.ch>
2025-06-18 01:32:36 -0600

    Change location to /app dir

--- htcondor/mnist_gpu_docker.sh
+++ htcondor/mnist_gpu_docker.sh
@@ -17,7 +17,7 @@ echo -e "# Check to see if the NVIDIA drivers can correctly detect the GPU:\n"
 nvidia-smi
 
 echo -e "\n# Check if PyTorch can detect the GPU:\n"
-python ./src/torch_detect_GPU.py
+python /app/src/torch_detect_GPU.py
 
 echo -e "\n# Extract the training data:\n"
 if [ -f "MNIST_data.tar.gz" ]; then
